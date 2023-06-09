df7900a7afb0d6da4ea9a55da5d4cd0a50f9e2e2
Eva <evalearns@protonmail.com>
2023-06-09 12:05:54 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+document.getElementById("get-activity").addEventListener("click", function() {
+    fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("activity").textContent = data.activity
+    })
+})
